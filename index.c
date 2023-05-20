0b1b32d9475ec420151dda1f4100b5f65b25a9f2
Minh718 <minhprokute85209@gmail.com>
2023-05-21 02:10:41 +0700

    oke

--- index.c
+++ index.c
@@ -5,7 +5,7 @@
 
 int main(){
 uint32_t *pgd = malloc(PAGING_CPU_BUS_WIDTH * sizeof(uint32_t));
-  for(pgit = 0; pgit < 10; pgit++)
+  for(int pgit = 0; pgit < 10; pgit++)
   {
      printf("%08ld: %08x\n", pgit * sizeof(uint32_t), pgd[pgit]);
   }
