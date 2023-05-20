362eb20758eb3f6317930aa9d78b975772351fcb
Minh718 <minhprokute85209@gmail.com>
2023-05-21 02:19:16 +0700

    oke

--- index.c
+++ index.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #define PAGING_CPU_BUS_WIDTH 22 /* 22bit bus - MAX SPACE 4MB */
 
 
 int main(){
-uint32_t *pgd = malloc(PAGING_CPU_BUS_WIDTH * sizeof(uint32_t));
+uint32_t *pgd;
+pgd = malloc(PAGING_CPU_BUS_WIDTH * sizeof(uint32_t));
   for(int pgit = 0; pgit < 10; pgit++)
   {
      printf("%08ld: %08x\n", pgit * sizeof(uint32_t), pgd[pgit]);
