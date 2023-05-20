eeede436e50bee79e8e3fc56da05d3f3071485be
Minh718 <91736257+Minh718@users.noreply.github.com>
2023-05-21 01:45:30 +0700

    Add files via upload

---
+++ index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(){
+uint32_t *pgd = malloc(PAGING_CPU_BUS_WIDTH * sizeof(uint32_t));
+  for(pgit = 0; pgit < 10; pgit++)
+  {
+     printf("%08ld: %08x\n", pgit * sizeof(uint32_t), pgd[pgit]);
+  }
+}
