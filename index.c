d667370a824c953bed147524f5e949c215aeee88
Minh718 <minhprokute85209@gmail.com>
2023-05-21 02:27:14 +0700

    oke

--- index.c
+++ index.c
@@ -3,13 +3,34 @@
 #include <stdlib.h>
 #include <stdint.h>
 #define PAGING_CPU_BUS_WIDTH 22 /* 22bit bus - MAX SPACE 4MB */
+#define SETBIT(v,mask) (v=v|mask)
+#define CLRBIT(v,mask) (v=v&~mask)
+#define SETVAL(v,value,mask,offst) (v=(v&~mask)|((value<<offst)&mask))
+#define PAGING_PTE_SWAPPED_MASK BIT(30)
+#define PAGING_PTE_PRESENT_MASK BIT(31)
+#define PAGING_PTE_FPN_LOBIT 0
+#define PAGING_PTE_FPN_HIBIT 12
+#define BITS_PER_LONG 32
+#define GENMASK(h, l) \
+	(((~0U) << (l)) & (~0U >> (BITS_PER_LONG  - (h) - 1)))
+#define PAGING_PTE_FPN_MASK    GENMASK(PAGING_PTE_FPN_HIBIT,PAGING_PTE_FPN_LOBIT)
+int pte_set_fpn(uint32_t *pte, int fpn)
+{
+  SETBIT(*pte, PAGING_PTE_PRESENT_MASK);
+  CLRBIT(*pte, PAGING_PTE_SWAPPED_MASK);
 
+  SETVAL(*pte, fpn, PAGING_PTE_FPN_MASK, PAGING_PTE_FPN_LOBIT); 
+
+  return 0;
+}
 
 int main(){
 uint32_t *pgd;
 pgd = malloc(PAGING_CPU_BUS_WIDTH * sizeof(uint32_t));
   for(int pgit = 0; pgit < 10; pgit++)
   {
+     uint32_t *pte = &pgd[pgit];
+     pte_set_fpn(pte, pgit);
      printf("%08ld: %08x\n", pgit * sizeof(uint32_t), pgd[pgit]);
   }
 }
