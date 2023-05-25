48d7bfe0a4086e111f64d9a5eed25b4401d5718b
Minh718 <minhprokute85209@gmail.com>
2023-05-25 12:21:20 +0700

    oke

--- index.c
+++ index.c
@@ -85,17 +85,17 @@ int pte_set_fpn(uint32_t *pte, int fpn)
 int main(){
     uint32_t *pgd = malloc(PAGING_CPU_BUS_WIDTH * sizeof(uint32_t));
     uint32_t *pte0 = &pgd[0];
-    uint32_t *pte1 = &pgd[1];
-    uint32_t *pte2 = &pgd[2];
+    // uint32_t *pte1 = &pgd[1];
+    // uint32_t *pte2 = &pgd[2];
     pte_set_swap(pte0, 0, 4);
-    pte_set_fpn(pte1, 1);
-    pte_set_fpn(pte2, 2);
+    // pte_set_fpn(pte1, 1);
+    // pte_set_fpn(pte2, 2);
     uint32_t e0 = pgd[0];
-    uint32_t e1 = pgd[1];
-    uint32_t e2 = pgd[2];
+    // uint32_t e1 = pgd[1];
+    // uint32_t e2 = pgd[2];
     printf("%d \n", PAGING_PAGE_PRESENT(e0));
-    printf("%d\n", PAGING_FPN(e1));
-    printf("%d\n", PAGING_FPN(e2));
+    // printf("%d\n", PAGING_FPN(e1));
+    // printf("%d\n", PAGING_FPN(e2));
 }
 
 
