f41f8af5bcb8186da8e1764036f86e3e956127e0
Minh718 <minhprokute85209@gmail.com>
2023-05-25 10:32:47 +0700

    oke

--- index.c
+++ index.c
@@ -132,12 +132,16 @@ int main() {
     uint32_t *pgd = malloc(PAGING_CPU_BUS_WIDTH * sizeof(uint32_t));
     uint32_t *pte0 = &pgd[0];
     uint32_t *pte1 = &pgd[1];
+    uint32_t *pte2 = &pgd[2];
     pte_set_fpn(pte0, 0);
     pte_set_fpn(pte1, 1);
+    pte_set_fpn(pte2, 2);
     uint32_t e0 = pgd[0];
     uint32_t e1 = pgd[1];
+    uint32_t e2 = pgd[2];
     printf("%d\n", PAGING_FPN(e1));
     printf("%d\n", PAGING_FPN(e0));
+    printf("%d\n", PAGING_FPN(e2));
 
     free(pgd); // Don't forget to free the allocated memory
     return 0;
